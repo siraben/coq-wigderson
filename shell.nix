c3957ac8ce60f60c3ee3b6ee374a1991253677b3
Ben Siraphob <bensiraphob@gmail.com>
2022-04-20 18:41:50 -0500

    Add nix environment

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ src ? import ./nix/sources.nix
+, pkgs ? import src.nixpkgs {} }:
+with pkgs;
+
+mkShell {
+  packages = [
+    coq_8_13
+    coqPackages_8_13.coqhammer
+    vampire
+    eprover
+    ((import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/a3e1e9271e0ff87309d44f9817baadb09b305757.tar.gz";
+    }) {}).cvc4)
+    (z3-tptp.overrideAttrs (oA: {
+      installPhase = oA.installPhase + ''
+    ln -s "z3_tptp5" "$out/bin/z3_tptp"
+  '';
+    }))
+  ];
+}
