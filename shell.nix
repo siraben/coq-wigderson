7d89baa67ae0630aa438ce577f8fe4cb3254585c
Ben Siraphob <bensiraphob@gmail.com>
2022-06-02 20:58:18 -0700

    update niv

--- shell.nix
+++ shell.nix
@@ -8,11 +8,7 @@ mkShell {
     coqPackages_8_13.coqhammer
     vampire
     eprover
-    (if stdenv.isDarwin then
-      ((import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/a3e1e9271e0ff87309d44f9817baadb09b305757.tar.gz";
-      }) {}).cvc4)
-    else cvc4)
+    cvc4
     (z3-tptp.overrideAttrs (oA: {
       installPhase = oA.installPhase + ''
     ln -s "z3_tptp5" "$out/bin/z3_tptp"
