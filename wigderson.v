7dd7b5b664f4efbe33bb26aa3eaf2d57fbde88bd
Ben Siraphob <bensiraphob@gmail.com>
2026-07-10 20:12:00 -0700

    wigderson: remove dead selectW cluster and unused lifting lemmas
    Drop selectW/selectW_terminates/subset_nodes_prop/cardinal_remove/
    select_hi_deg (only supported the unused select_hi_deg), phase2_0,
    no_selfloop_dec, phase_2_example, adj_subset_nodes, fold_left_max_mono,
    two_color_up_inj, three_color_up_inj, and stale trailing dev comments.

--- wigderson.v
+++ wigderson.v
@@ -36,121 +36,24 @@ Local Open Scope positive_scope.
     Phase 2 is coloring the remaining nodes with at most sqrt(k) colors.
  *)
 
-(** ** Termination of selectW *)
-Lemma selectW_terminates:
-  forall (K: nat) (g : graph) (n : S.elt),
-   S.choose (subset_nodes (high_deg K) g) = Some n ->
-   (M.cardinal (remove_node n g) < M.cardinal g)%nat.
-Proof.
-  intros K g n H.
-  unfold remove_node.
-  assert (~ M.In n (remove_node n g)) by strivial use: in_remove_node_iff.
-  assert (M.In n g).
-  {
-    assert (S.In n (subset_nodes (high_deg K) g))
-      by hauto l: on use: S.choose_1.
-    pose proof (subset_nodes_sub (high_deg K) g n H1).
-    unfold nodes, Mdomain in H2.
-    now apply in_domain in H2.
-  }
-  rewrite cardinal_map.
-  now apply m_remove_cardinal_less.
-Qed.
+(** * Phase 1: color the neighborhoods of high-degree vertices
 
-Function selectW (K: nat) (g: graph) {measure M.cardinal g} : list node :=
-  match S.choose (subset_nodes (high_deg K) g) with
-  | Some n => n :: selectW K (remove_node n g)
-  | None => nil
-  end.
-Proof. apply selectW_terminates.
-Defined.
+    While a high-degree vertex [v] (degree > [k]) exists, 2-color its
+    neighborhood [N(v)] with two fresh colors, color [v] with a third,
+    remove [v] and [N(v)] from the graph, and recurse. Each step uses
+    three new colors and removes at least [k+2] vertices. *)
 
-(** ** Property of subset_nodes *)
-Lemma subset_nodes_prop : forall (P: node -> nodeset -> bool) (g: graph) v,
-    S.In v (subset_nodes P g) -> P v (adj g v) = true.
-Proof.
-  intros P g v H.
-  unfold subset_nodes in H.
-  apply in_domain in H.
-  destruct H as [e He].
-  epose proof (@WP.filter_iff _ P _ g v e).
-  rewrite H in He.
-  unfold adj.
-  sauto.
-Qed.
-
-(* If a node m is removed from the graph then the cardinality of the
-  adj set of a vertex v is decreasing. *)
-(** ** Cardinality of adjacency set after removing a node *)
-Lemma cardinal_remove : forall g v m,
-    (S.cardinal (adj (remove_node m g) v) <= S.cardinal (adj g v))%nat.
-Proof.
-  intros g v m.
-  unfold adj.
-  destruct (WF.In_dec g v).
-  - ssimpl.
-    subst.
-    unfold nodeset, node, remove_node in *.
-    assert (n0 = x) by scongruence.
-    subst.
-    (* Note: why is this not more automatic *)
-    destruct (E.eq_dec v m).
-    + ecrush use: map_o, M.grs.
-    + unfold nodeset in *.
-      rewrite map_o, M.gro in Heqo by auto.
-      qauto use: SP.subset_cardinal, PositiveSet.remove_3 unfold: PositiveSet.elt, PositiveOrderedTypeBits.t, option_map, PositiveSet.Subset.
-  - hauto lq: on use: remove_node_subgraph, SP.subset_cardinal unfold: PositiveMap.In, PositiveSet.t, is_subgraph, nodeset, PositiveMap.MapsTo, PositiveSet.Subset, adj inv: option.
-Qed.
-
-(* If v is in the list returned by selectW then the cardinality of v is indeed high. *)
-(** ** Vertices selected by selectW have high degree *)
-Lemma select_hi_deg : forall n g v, In v (selectW n g) -> (S.cardinal (adj g v) > n)%nat.
-Proof.
-  intros n g v.
-  functional induction (selectW n g) using selectW_ind.
-  - intros H.
-    destruct H.
-    + subst.
-      clear IHl.
-      assert (S.In v (subset_nodes (high_deg n) g)).
-      {
-        hauto q: on use: PositiveSet.choose_1 inv: option.
-      }
-      hauto lqb: on drew: off use: subset_nodes_prop unfold: high_deg.
-    + specialize (IHl H).
-      hfcrush use: cardinal_remove unfold: PositiveOrderedTypeBits.t, PositiveSet.elt, PositiveSet.t, adj, node, nodeset, PositiveSet.empty.
-  - sfirstorder.
-Qed.
-
-(* Phase 1 of Wigderson *)
-(* Let:
-- k: bound for degree of vertices
-- c: color currently used
-- g: graph
-- f: coloring
-- H: proof that g is 3-colorable
-- H1: proof that f is a complete coloring of g
-- Hf: proof that f is a 3-coloring
-
-phase1(g,c):
-  if a high degree vertex in g exists:
-    v := select a high-degree vertex in g
-    nbhd := neighborhood of v in g
-    coloring_of_nbhd := 2-coloring of nbhd using c+1 and c+2
-    g' := remove nbhd from g
-    return union(coloring_of_nbhd, phase1(g',c+2))
-  else:
-    return empty coloring
-
- *)
 Require Import Program.
 
-(* Two-coloring of a neighborhood using BFS-based forcing *)
+(** 2-coloring of the neighborhood of [v] using colors [c1], [c2],
+    computed by BFS-based forcing. *)
 Definition two_color_nbd (g : graph) (v : node) (c1 c2 : positive) : coloring :=
   force_all (neighborhood g v) c1 c2.
 
-(* Two coloring of a neighborhood of a 3-colorable graph is complete *)
-
+(** The recursive body of phase 1: at each step it picks a high-degree
+    vertex, 2-colors its neighborhood, colors the vertex itself, and
+    recurses on the residual graph. Returns the coloring and the
+    residual graph of untouched vertices. *)
 Function phase1
   (k : nat) (c : positive) (g : graph) {measure M.cardinal g} : coloring * graph :=
   match S.choose (subset_nodes (high_deg k) g) with
@@ -521,16 +424,6 @@ Proof.
   destruct (Pos.compare_spec v a); destruct (Pos.compare_spec v b); lia.
 Qed.
 
-Lemma fold_left_max_mono : forall l (acc1 acc2 : positive),
-  (acc1 <= acc2)%positive ->
-  (fold_left (fun (a : positive) (p : M.key * positive) => Pos.max (snd p) a) l acc1 <=
-   fold_left (fun (a : positive) (p : M.key * positive) => Pos.max (snd p) a) l acc2)%positive.
-Proof.
-  induction l as [|[k v] l IH]; intros acc1 acc2 H; simpl.
-  - auto.
-  - apply IH. apply Pos_max_le_compat_r. auto.
-Qed.
-
 Lemma fold_left_max_le_acc : forall l (acc : positive),
   (acc <= fold_left (fun (a : positive) (p : M.key * positive) => Pos.max (snd p) a) l acc)%positive.
 Proof.
@@ -640,62 +533,8 @@ Defined.
 
 Functional Scheme phase2_ind := Induction for phase2 Sort Prop.
 
-(** ** Phase 2 on max degree 0 graphs *)
-Lemma phase2_0 (g : graph) : max_deg g = 0%nat -> phase2 g = (constant_color (nodes g) 1, @M.empty _).
-Proof.
-  intros H.
-  hauto lq: on use: phase2_equation.
-Qed.
-
-Lemma no_selfloop_dec (g : graph) : {no_selfloop g} + {~ no_selfloop g}.
-Proof.
-  unfold no_selfloop.
-  remember (M.elements g) as l.
-  refine (_ (Exists_dec (fun (p : M.key * nodeset) => let (a,b) := p in S.In a b) l _)).
-  - intros H.
-    destruct H.
-    + right.
-      intros contra.
-      apply Exists_exists in e.
-      destruct e as [[k n] [Hx Hx']].
-      unfold adj in contra.
-      rewrite Heql in Hx.
-      pose proof (contra k).
-      apply H.
-      apply M.elements_complete in Hx.
-      hauto lq: on.
-    + left.
-      intros i contra.
-      apply n.
-      apply Exists_exists.
-      unfold adj in contra.
-      destruct (M.find i g) eqn:E; [|sauto].
-      exists (i, n0).
-      sfirstorder use: M.elements_correct.
-  - hauto lq: on use: SP.In_dec.
-Defined.
-
-Example phase_2_example : coloring_ok (SP.of_list [1;2;4]) ex_graph (fst (phase2 ex_graph)).
-Proof.
-  split.
-  - intros ci Hci.
-    apply M.elements_correct in Hci.
-    compute in Hci.
-    hauto l: on.
-  - intros ci cj Hci Hcj.
-    apply M.elements_correct in Hci, Hcj.
-    compute in Hci.
-    compute in Hcj.
-    hauto l: on.
-Qed.
-
-(* Next steps:
- - how can we know a-priori what colors are used in phase2?
- - prove that the coloring returned by phase2 is ok
- - actually we're trying to prove that phase2 uses _at most_ max_deg + 1 colors
- - the coloring_ok predicate can overapproximate
-*)
-
+(** The palette [{1, ..., p+1}] as a node set, used to bound the colors
+    produced by phase 2. *)
 Definition siota p := SP.of_list (map Pos.of_nat (seq 1 (p + 1))).
 Definition phase2_colors g := siota (max_deg g).
 
@@ -1164,13 +1003,6 @@ Proof.
   pose proof (SP.union_inter_cardinal (S.diff A B) B). lia.
 Qed.
 
-(** Adjacency set is a subset of nodes for undirected graphs *)
-Lemma adj_subset_nodes : forall g v,
-  undirected g -> S.Subset (adj g v) (nodes g).
-Proof.
-  intros g v Ug x Hx. eapply in_adj_neighbor_in_nodes; eauto.
-Qed.
-
 (** Each phase1 step removes at least k+2 vertices *)
 Lemma phase1_removes_many : forall k g v,
   undirected g -> no_selfloop g ->
@@ -1343,179 +1175,3 @@ Proof.
   pose proof (wigderson_color_bound (Nat.sqrt (M.cardinal g)) g i ci Ug Hloop Hfi) as H.
   pose proof (sqrt_div_le (M.cardinal g)). lia.
 Qed.
-
-(* Some notes about how the algorithm will be written:
-
-- we will not pass proofs that the graph is 3-colorable all the time,
-  since later we want to talk about robustness
-
-- we want to break down algorithm enough to reason about the graph and
-  the coloring at each step while still making the overall algorithm
-  clear
-
-- an important lemma we will use repeatedly is that we can combine OK
-  disjoint colorings into larger colorings, and that the number of
-  colors used is the size of the palette
-
-- to show that the coloring is complete, we show that a vertex must be
-  one of the following:
-
-1) a high-degree vertex, in which case it's colored by phase 1
-2) a neighbor of a high-degree vertex, in which case it's colored by phase 1
-3) a low-degree vertex, in which case it's colored by phase 2
- *)
-
-(*
-We define phase 1 of the algorithm in several pieces:
-
-two_color_step takes:
-  - a graph g
-  - a high-degree vertex v
-  - the current color number c
-two_color_step outputs:
-  - a complete coloring of N(v) using colors c+1, c+2
-note:
-  - phase 1 will color v with c
-  - the expectation is that two_color_step will be called with another
-    vertex and c+2
-
-so phase 1 will fold over the list of high-degree vertices Lv and do:
-- the state of the fold will be (c,m), where c is a number and m is the coloring
-- call two_color_step on each vertex to produce the coloring and update m
-- update c to c+2
-
-we will have to show that:
-
-- the resulting tuple will be (d,m), where d is the number of high-degree
-  vertices and m is the coloring of all the neighborhoods of high-degree vertices
-- d*d <= k
-- the number of colors used is 2*d
-
-random lemmas that we have to show:
-- it's decidable to check whether a node is colored or not (WF.In_Dec)
-- the number of high-degree vertices cannot exceed sqrt(k)
- *)
-
-(* Lifting two color maps with any injective function *)
-(** ** Lifting a 2-coloring by an injective function *)
-Lemma two_color_up_inj f g (inj : S.elt -> S.elt) :
-  injective inj ->
-  undirected g ->
-  coloring_ok two_colors g f ->
-  {h | coloring_ok (fold_right S.add S.empty [inj 1;inj 2]) g h}.
-Proof.
-  intros Hm Ug Hf.
-  exists (M.map inj f).
-  intros v.
-  split.
-  - intros ci Hv.
-    assert (M.In v f) by hauto l: on use: M.map_2.
-    destruct H0 as [cj Hcj].
-    unfold M.MapsTo in Hcj.
-    assert (ci = inj 1 \/ ci = inj 2).
-    {
-      unfold coloring_ok in Hf.
-      pose proof (map_o f v inj).
-      assert (Some ci = option_map inj (M.find v f)) by qauto l: on.
-      sauto q: on.
-    }
-    destruct H0; hauto use: PositiveSet.add_2, PositiveSet.add_1 unfold: PositiveSet.elt.
-  - intros ci cj Hci Hcj.
-    assert (M.In v f) by hauto l: on use: M.map_2.
-    assert (M.In j f) by hauto l: on use: M.map_2.
-    destruct H0 as [ci' Hci'].
-    destruct H1 as [cj' Hcj'].
-    unfold M.MapsTo in *.
-    assert (Some ci = option_map inj (M.find v f)) by sauto lq: on use: map_o.
-    assert (Some cj = option_map inj (M.find j f)) by sauto lq: on use: map_o.
-    pose proof (proj2 (Hf v j H) ci' cj' Hci' Hcj').
-    assert (ci' = 1 \/ ci' = 2).
-    {
-      pose proof (proj1 (Hf _ _ H) _ Hci').
-      sauto.
-    }
-    (* Here we use the fact that the graph is undirected so we can
-       go from j being adjacent to v to v being adjacent to j. *)
-    assert (cj' = 1 \/ cj' = 2).
-    {
-      pose proof (proj1 (Hf _ _ (Ug _ _ H)) _ Hcj').
-      sauto.
-    }
-    sauto.
-Qed.
-
-(* Lifting three color maps *)
-(** ** Lifting a 3-coloring by an injective function *)
-Lemma three_color_up_inj f g (inj : S.elt -> S.elt) :
-  injective inj ->
-  undirected g ->
-  coloring_ok three_colors g f ->
-  {h | coloring_ok (fold_right S.add S.empty [inj 1;inj 2;inj 3]) g h}.
-Proof.
-  intros Hm Ug Hf.
-  exists (M.map inj f).
-  intros v.
-  split.
-  - intros ci Hv.
-    assert (M.In v f) by hauto l: on use: M.map_2.
-    destruct H0 as [cj Hcj].
-    unfold M.MapsTo in Hcj.
-    assert (ci = inj 1 \/ ci = inj 2 \/ ci = inj 3).
-    {
-      unfold coloring_ok in Hf.
-      pose proof (map_o f v inj).
-      assert (Some ci = option_map inj (M.find v f)) by qauto l: on.
-      sauto q: on.
-    }
-    destruct H0; hauto use: PositiveSet.add_2, PositiveSet.add_1 unfold: PositiveSet.elt.
-  - intros ci cj Hci Hcj.
-    assert (M.In v f) by hauto l: on use: M.map_2.
-    assert (M.In j f) by hauto l: on use: M.map_2.
-    destruct H0 as [ci' Hci'].
-    destruct H1 as [cj' Hcj'].
-    unfold M.MapsTo in *.
-    assert (Some ci = option_map inj (M.find v f)) by sauto lq: on use: map_o.
-    assert (Some cj = option_map inj (M.find j f)) by sauto lq: on use: map_o.
-    pose proof (proj2 (Hf v j H) ci' cj' Hci' Hcj').
-    assert (ci' = 1 \/ ci' = 2 \/ ci' = 3).
-    {
-      pose proof (proj1 (Hf _ _ H) _ Hci').
-      sauto.
-    }
-    (* Here we use the fact that the graph is undirected so we can
-       go from j being adjacent to v to v being adjacent to j. *)
-    assert (cj' = 1 \/ cj' = 2 \/ cj' = 3).
-    {
-      pose proof (proj1 (Hf _ _ (Ug _ _ H)) _ Hcj').
-      sauto.
-    }
-    sauto.
-Qed.
-
-
-(* To make "without loss of generality" type arguments, we'll need to
-turn any 2-coloring into ones that involve just the colors 1,2 then
-use appropriate lemmas to turn them into c, c+1.
-*)
-
-(* Idea: use reflection for coloring_ok *)
-
-(* Remove whole neighborhood from a graph *)
-
-(* Try to abstract the details about the criterion about how the
-   high-degree vertex is chosen *)
-
-(*
-Try to 2-color a 2-colorable graph
-
-- choose a vertex, color it 1
-- color all of its neighbors 2
-- when you color a vertex 2, color all of its neighbors 1
-- keep going
-
- *)
-
-(*
-Induction principle for graphs?
-
-*)
